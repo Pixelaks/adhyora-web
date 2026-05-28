05fe6a1a09faa2ecbd33a8d9a69b66519ecf132f
ANAGH K SATHEESH <86482414+anaghksatheesh@users.noreply.github.com>
2026-05-28 16:08:48 +0530

    Update version-check.js

--- version-check.js
+++ version-check.js
@@ -20,7 +20,7 @@ remoteConfig.settings = {
 // ==========================================
 // 🚨 CHANGE ONLY THIS VERSION
 // ==========================================
-const LOCAL_VERSION = "1.0.56";
+const LOCAL_VERSION = "1.0.57";
 
 // ==========================================
 // SHOW VERSION TEXT
