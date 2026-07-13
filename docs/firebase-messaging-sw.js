0acd916d918c608d7764c2a6e35233c0efa4f010
Anagh K Satheesh <anaghks03@gmail.com>
2026-07-13 15:14:12 +0530

    notification bug fix

--- docs/firebase-messaging-sw.js
+++ docs/firebase-messaging-sw.js
@@ -91,8 +91,9 @@ messaging.onBackgroundMessage((payload) => {
 
   const notificationOptions = {
     body: body,
-    icon:  './ic_stat_notify.png',
-    badge: './ic_stat_notify.png',
+    icon:   './ic_stat_notify.png',
+    badge:  './ic_stat_notify.png',
+    image:  payload.data.image || './web-app-manifest-192x192.png',
     vibrate: [200, 100, 200],
     requireInteraction: true,
     tag: type,
